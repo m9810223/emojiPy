c92f669e7cf5998dbdb35c60b1c20f8efadc840a
m9810223 <m9810223@gmail.com>
2022-02-09 12:39:37 +0800

    :bookmark: build: 2022-02-09

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
-from sys import stdin, stdout, argv
+import sys
 from re import findall
 from random import choice
 
 from emojies import emojies
 
-if len(argv) == 1:
-    for line in stdin:
-        for e in findall(pattern=':.+?:', string=line):
-            if e in emojies:
-                line = line.replace(e, emojies[e])
-        stdout.write(line)
-elif argv[1] in ('-l', '--list'):
-    for k, v in emojies.items():
-        print(v, k)
-elif argv[1] in ('-r', '--random'):
-    print(choice(list(emojies.values())))
-else:
-    print('Faster 🚀 Python 🐍 emojify 🤗')
-    print()
-    print('Usage:  emojiPy [OPTIONS] [INPUT]')
-    print()
-    print('Options:')
-    print('    -l, --list     List all emojies.')
-    print('    -r, --random   Print a random emoji.')
+
+def main():
+    if len(sys.argv) == 1:
+        for line in sys.stdin:
+            for e in findall(pattern=':.+?:', string=line):
+                if e in emojies:
+                    line = line.replace(e, emojies[e])
+            try:
+                print(line, end='')
+            except Exception as e:
+                print(e)
+    elif sys.argv[1] in ('-l', '--list'):
+        for k, v in emojies.items():
+            print(v, k)
+    elif sys.argv[1] in ('-r', '--random'):
+        print(choice(list(emojies.values())))
+    else:
+        print('Faster 🚀 Python 🐍 emojify 🤗')
+        print()
+        print('Usage:  emojiPy [OPTIONS] [INPUT]')
+        print()
+        print('Options:')
+        print('    -l, --list     List all emojies.')
+        print('    -r, --random   Print a random emoji.')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        # https://docs.python.org/3/library/signal.html#note-on-sigpipe
+        sys.stdout.flush()
+    except BrokenPipeError:
+        sys.stdout = None
